2b30147a3f1d1208f1836f46a2a19d04ca00f88a
José Cardoso <caardoso4@outlook.com>
2023-07-18 07:00:22 -0300

    new file

---
+++ app.js
@@ -0,0 +1,105 @@
+const openCart = document.querySelector(".shop-bag");
+const closeCart = document.querySelector(".close-btn");
+const foodCont = document.querySelector(".food-container");
+const foodList = document.querySelector(".food-list");
+const totalSpend = document.querySelector(".total");
+const quantProd = document.querySelector(".shop-bag-counter");
+const body = document.querySelector("body");
+
+openCart.addEventListener("click", ()=>{
+    body.classList.add("active");
+})
+
+closeCart.addEventListener("click", ()=>{
+    body.classList.remove("active");
+})
+
+const products = [
+    {
+        id: 1,
+        name: "PRODUCT NUMBER ONE",
+        img: "images/1.png",
+        price: 30.00,
+    },
+    {
+        id: 2,
+        name: "FRANGUINHO DUCO",
+        img: "images/2.png",
+        price: 35.00,
+    },
+    {
+        id: 3,
+        name: "PRODUCT NUMBER THREE",
+        img: "images/3.png",
+        price: 40.00,
+    },
+    {
+        id: 4,
+        name: "PRODUCT NUMBER FOUR",
+        img: "images/4.png",
+        price: 27.00,
+    },
+    {
+        id: 5,
+        name: "PRODUCT NUMBER FIVE",
+        img: "images/5.png",
+        price: 32.00,
+    },
+    {
+        id: 6,
+        name: "PRODUCT NUMBER SIX",
+        img: "images/6.png",
+        price: 50.00,
+    }
+];
+
+let listCarts = [];
+
+function initApp(){
+    products.forEach((value, key) => {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("item");
+        newDiv.innerHTML = `
+            <img src="${value.img}"/>
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString('pt-BR', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            })}</div>
+            <button onclick="addToCart(${key})">ADD TO CART</button>
+        `;
+        foodCont.appendChild(newDiv);
+    })
+}
+initApp();
+
+function addToCart(key){
+    if (listCarts == null){
+        listCarts[key] = products[key];
+        listCarts[key].quantProd = 1;
+    }
+    reloadCart();
+}
+
+function reloadCart(){
+    foodList.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+
+    listCarts.forEach((value, key) => {
+        totalPrice += value.price;
+        count += value.quantProd;
+        if (value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="${value.img}/></div>
+                <div>${value.name}</div>
+                <div>${value.price}</div>
+                <div>${value.quantProd}</div>
+            `;
+            foodList.appendChild(newDiv);
+        }
+    })
+    totalSpend.innerText = totalPrice;
+    quantProd.innerText = count;
+}
